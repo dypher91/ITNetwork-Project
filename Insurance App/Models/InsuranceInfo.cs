f7309b4b828d980458ab4b0220a157ae4f27bd45
Pavel Cicu <39915158+dypher91@users.noreply.github.com>
2023-04-27 15:18:51 +0200

    models and obj

---
+++ Insurance App/Models/InsuranceInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PojisteniApp.Models
+{
+    public class InsuranceInfo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Popis")]
+        [Required(ErrorMessage = "Popis je nutný")]
+        [StringLength(maximumLength: 100)]
+        public string DescriptionOfInsurance { get; set; } = "";
+
+        public int DescriptionId { get; set; }
+    }
+}
