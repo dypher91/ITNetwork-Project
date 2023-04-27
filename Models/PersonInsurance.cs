a06f2b271c7e060ab254f53e299e3e8e33451b95
Pavel Cicu <39915158+dypher91@users.noreply.github.com>
2023-04-27 15:02:16 +0200

    models

---
+++ Models/PersonInsurance.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PojisteniApp.Models
+{
+    public class PersonInsurance
+    {
+        [Key]
+        public int PersonInsuranceId { get; set; }
+
+        [Display(Name = "Částka")]
+        [Required(ErrorMessage = "Částka je nutná")]
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "Only numbers are allowed.")]
+        public int ValueOfInsurance { get; set; }
+
+        [Display(Name = "Předmět pojištění")]
+        [Required(ErrorMessage = "Nutné")]
+        [RegularExpression(@"^[a-zA-Zá-žÁ-Ž]+$", ErrorMessage = "Only letters are allowed.")]
+        public string IntrestOfInsurance { get; set; } = "";
+
+        [Display(Name = "Datum od")]
+        [Required(ErrorMessage = "Nutné")]
+        //[RegularExpression(@"^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$", ErrorMessage = "Date is invalid.")]
+        public DateTime InsuranceStart { get; set; }
+
+
+        [Display(Name = "Datum do")]
+        [Required(ErrorMessage = "Nutné")]
+        //[RegularExpression(@"^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$", ErrorMessage = "Date is invalid.")]
+        public DateTime InsuranceEnd { get; set; }
+
+
+        [Display(Name = "Rodné číslo")]
+        [Required(ErrorMessage = "RČ je nutné")]
+        public int PersonId { get; set; }
+        public InsuracePersonData? Person { get; set; }
+    }
+}
